9bad693d11eb421862941e29e634206816a5a5bd
Jacob Froman <jacob.h.froman@gmail.com>
2016-12-18 22:19:36 -0600

    table creates

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -1,9 +1,24 @@
 -- Database Name: todos
 
 -- Create table
-CREATE TABLE todos (
+CREATE TABLE list (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE person (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE task (
     id SERIAL PRIMARY KEY,
     name VARCHAR(200) NOT NULL,
-    description TEXT,
-    complete BOOLEAN DEFAULT FALSE
+    complete BOOLEAN DEFAULT FALSE,
+    list_id INTEGER REFERENCES list(id)
+);
+
+CREATE TABLE person_task (
+    person_id INTEGER REFERENCES person(id) ON DELETE CASCADE,
+    task_id INTEGER REFERENCES task(id) ON DELETE CASCADE
 );
